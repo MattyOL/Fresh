70acbf8944710d75e3254232c8d5ea9d861b3d68
MattyOL <mattyoleary1997@gmail.com>
2023-07-14 11:44:59 +0000

    added env.py file

--- freshfragrance/settings.py
+++ freshfragrance/settings.py
@@ -13,6 +13,15 @@ import os
 
 from pathlib import Path
 
+import os
+
+
+if os.path.exists("env.py"):
+    import env 
+
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
