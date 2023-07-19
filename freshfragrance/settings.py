68a75fe2fdf4753af3664e0b1396187ebda50cac
MattyOL <mattyoleary1997@gmail.com>
2023-07-19 11:52:12 +0000

    Added more products/imgs, testing different imgs for homepage

--- freshfragrance/settings.py
+++ freshfragrance/settings.py
@@ -34,8 +34,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+# SECURITY WARNING: don't run with debug turned on in production!  #'DEVELOPMENT' in os.environ
+DEBUG = True  
 
 ALLOWED_HOSTS = ['8000-mattyol-fresh-wox1tw2jfil.ws-eu101.gitpod.io', 'localhost', 'fresh-p5-3dd3ff854c84.herokuapp.com' ]
 
@@ -144,9 +144,9 @@ else:
        }
    }
 
-DATABASES = {
-    'default': dj_database_url.parse('postgres://omoswitc:n-ntZfPcyAyYeVGJdNKUOWtr0d0BcoWa@horton.db.elephantsql.com/omoswitc')
-}
+#DATABASES = {
+#    'default': dj_database_url.parse('postgres://omoswitc:n-ntZfPcyAyYeVGJdNKUOWtr0d0BcoWa@horton.db.elephantsql.com/omoswitc')
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
