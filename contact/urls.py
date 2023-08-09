9c9b2aa806441cecd04be8efaf6d1559b18b124d
MattyOL <mattyoleary1997@gmail.com>
2023-08-09 07:47:22 +0000

    Fixed incorrect url path

--- contact/urls.py
+++ contact/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.contact, name='contact'),
-    path('contact/', views.contact, name='contact'),
   
 ]
--- wishlist/urls.py
+++ wishlist/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('wishlist/templates/wishlist/wishlist.html', views.wishlist, name='wishlist'),
+    path('', views.wishlist, name='wishlist'),
     path('add-to-wishlist/<int:item_id>/', views.add_to_wishlist, name='add_to_wishlist'),
     path('remove/<item_id>/', views.remove_from_wishlist, name='remove_from_wishlist'),
     path('add/<item_id>/', views.add_to_bag, name='add_to_bag'),
