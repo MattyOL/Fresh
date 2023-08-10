bcfa51205d0c7ec48e1745007a59078985070f47
MattyOL <mattyoleary1997@gmail.com>
2023-08-10 11:20:42 +0000

    Deloyed to heroku

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+# products page 
+
+
+class HomepageTests(SimpleTestCase):
+    databases = ['default']
+
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse("product"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_name_correct(self):
+        response = self.client.get(reverse("product"))
+        self.assertTemplateUsed(response, "product.html")
+
--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+# Profile
+
+
+class HomepageTests(SimpleTestCase):
+    databases = ['default']
+
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse("profile"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_name_correct(self):
+        response = self.client.get(reverse("profile"))
+        self.assertTemplateUsed(response, "profile.html")
+
+
