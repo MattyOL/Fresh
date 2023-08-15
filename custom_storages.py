1a6bcea5856b12db52193ecf59312c71d85d59d4
MattyOL <mattyoleary1997@gmail.com>
2023-08-15 08:50:55 +0000

    fixed s3 crashing on click static

--- custom_storages.py
+++ custom_storages.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from storages.backends.s3boto3 import S3Boto3Storage
+from storages.backends.s3boto3 import S3Boto3Storage, ManifestFilesMixin, ClientError, FileNotFoundError
 
 
 class StaticStorage(S3Boto3Storage):
@@ -8,3 +8,16 @@ class StaticStorage(S3Boto3Storage):
 
 class MediaStorage(S3Boto3Storage):
     location = settings.MEDIAFILES_LOCATION
+
+
+# https://github.com/jschneier/django-storages/issues/841
+class ManifestS3Boto3Storage(ManifestFilesMixin, S3Boto3Storage):
+    def _open(self, name, mode='rb'):
+        name = self._normalize_name(self._clean_name(name))
+        try:
+            f = S3Boto3StorageFile(name, mode, self)
+        except ClientError as err:
+            if err.response['ResponseMetadata']['HTTPStatusCode'] == 404:
+                raise FileNotFoundError('File does not exist: %s' % name)
+            raise  # Let it bubble up if it was some other error
+        return f
